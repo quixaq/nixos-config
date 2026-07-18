eeb2c9f3953f3022fdf33a37ff03534214e6fa9f
Quixaq <quixaq@tutamail.com>
2026-07-18 16:28:03 +0200

    chore: disable non functional auto upgrade

--- modules/nix.nix
+++ modules/nix.nix
@@ -17,16 +17,6 @@
 
   # system
   system = {
-    autoUpgrade = {
-      enable = true;
-      flake = inputs.self.outPath;
-      flags = [ "-L" ];
-      dates = "daily";
-      persistent = true;
-      allowReboot = false;
-      runGarbageCollection = true;
-    };
-
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
